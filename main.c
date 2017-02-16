ac270259799cf2736bf01232f3245e583b7d467d
Соколов Кирилл Андреевич <sokol0198@yandex.ru>
2017-02-16 15:09:58 +0700

    Project Stage 2

--- main.c
+++ main.c
@@ -4,9 +4,22 @@ int main ()
 { int summa; int srok; // Размер вклада и срок
   printf ("Введите сумму и срок \n");
   scanf ("%d %d",&summa,&srok);
-  if (summa > 10000 && srok > 0 && srok < 31)
-  {summa = summa - (summa / 100 * 10);
+  if (summa > 9999 && srok > 0 && srok < 31) // Вклад 1
+  {
+      (summa = summa - summa / 100 * 10);
       printf("Сумма Равна =  %d\n", summa);
   }
+  if (summa > 9999 && summa < 100000 && srok > 30 && srok < 121) // Вклад 2
+      
+  {
+      (summa = summa + summa / 100 * 2);
+      printf("Сумма Равна =  %d\n", summa);
+  }
+      else if (summa > 99999 && srok > 30 && srok < 121)
+      {
+          (summa = summa + summa / 100 * 3);
+      printf("Сумма Равна =  %d\n", summa);
+  }
+   
   return 0;
 }
