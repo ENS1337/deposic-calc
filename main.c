386ff7b6d5d1ab5bc84251613671aac359d36ef6
Соколов Кирилл Андреевич <sokol0198@yandex.ru>
2017-02-16 14:33:53 +0700

    If else 1 stage

--- main.c
+++ main.c
@@ -4,5 +4,9 @@ int main ()
 { int summa; int srok; // Размер вклада и срок
   printf ("Введите сумму и срок \n");
   scanf ("%d %d",&summa,&srok);
+  if (summa > 10000 && srok > 0 && srok < 31)
+  {summa = summa - (summa / 100 * 10);
+      printf("Сумма Равна =  %d\n", summa);
+  }
   return 0;
 }
