02d215503a6b7da2f50bbb77b7db0a91b21e91b1
ENS1337 <sokol0198@yandex.ru>
2017-03-02 08:54:06 +0700

    Redacted Code v3

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
-int main ()
-{ int summa; int srok; // Размер вклада и срок
+int percent (int summa; int srok) // Размер вклада и срок
+{
   printf ("Введите сумму не менее 10000р и срок не более 365 дней \n");
   scanf ("%d %d",&summa,&srok);
   if (summa > 9999 && srok > 0 && srok < 31) // Вклад 1
@@ -39,5 +39,26 @@ int main ()
           (summa = summa + summa / 100 * 15);
       printf("Сумма Равна =  %d\n", summa);
   }
+
+int check(int srok, long int summa) {   
+    int k=0;
+    if ((srok < 0) || (srok > 365) || (summa < 10000))  k = 0;
+    else k = 1;							  
+    return(k);
+}
+
+int main() {
+    int srok = 0, l = 0;
+    long int summa = 0;
+    printf("enter the term of the deposit\n");
+    scanf("%d", &srok);
+    printf("enter the deposit bag\n");
+    scanf("%ld", &summa);
+    l=check(srok, summa);
+    if (l==1) printf("your income %ld\n", percent(srok, summa));
+    else printf("incorrect data entry\n");
+    return 0;
+}
+
 return 0;
 }
