bca9d92b78d9f6fda8ecf12f8f6a63a8d461713e
ENS1337 <sokol0198@yandex.ru>
2017-04-05 18:29:14 +0700

    Adding folders and .gitkeep

---
+++ src/deposit.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+#include <deposit.h>
+
+short int is_correct_number(char *a) // Проверка правильности ввода
+{
+    for (int i = 0; i < strlen(a) - 1; i++) {
+        if (isdigit((int) a[i]) == 0) {
+            return 0;
+        }
+    }
+    return 1;
+}
