c3469b9a22bc418e34f02cc770cf9841fbf55a0c
ENS1337 <sokol0198@yandex.ru>
2017-04-05 19:05:41 +0700

    Deleting the input validation function

--- src/main.c
+++ src/main.c
@@ -2,16 +2,7 @@
 #include <ctype.h>
 #include <string.h>
 #include <stdlib.h>
-
-short int is_correct_number(char *a) // Проверка правильности ввода
-{
-    for (int i = 0; i < strlen(a) - 1; i++) {
-        if (isdigit((int) a[i]) == 0) {
-            return 0;
-        }
-    }
-    return 1;
-}
+#include <deposit.h>
 
 int main() {
     int summa;
