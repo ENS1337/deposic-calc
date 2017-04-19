fb8e3809cc7e732ea12e67abe05d22921e9bde1e
ENS1337 <sokol0198@yandex.ru>
2017-04-19 18:27:32 +0700

    Made code style

--- src/deposit.c
+++ src/deposit.c
@@ -4,23 +4,22 @@
 #include <stdlib.h>
 #include "deposit.h"
 
-int Bklad (int summa,int srok)
-{
+int Bklad(int summa, int srok) {
     if (summa > 9999 && srok > 0 && srok < 31) // Вклад 1
     {
-      summa = summa - summa / 100 * 10;
+        summa = summa - summa / 100 * 10;
     }
     if (summa > 9999 && summa < 100000 && srok > 30 && srok < 121) // Вклад 2
     {
-      summa = summa + summa / 100 * 2;
+        summa = summa + summa / 100 * 2;
     } else if (summa > 99999 && srok > 30 && srok < 121) {
-      summa = summa + summa / 100 * 3;
+        summa = summa + summa / 100 * 3;
     }
     if (summa > 9999 && summa < 100000 && srok > 120 && srok < 241) // Вклад 3
     {
-      summa = summa + summa / 100 * 6;
+        summa = summa + summa / 100 * 6;
     } else if (summa > 99999 && srok > 120 && srok < 241) {
-      summa = summa + summa / 100 * 8;
+        summa = summa + summa / 100 * 8;
     }
     if (summa > 9999 && summa < 100000 && srok > 240 && srok < 366) // Вклад 4
     {
@@ -30,11 +29,10 @@ int Bklad (int summa,int srok)
     }
     return summa;
 }
-int check (int summa, int srok) {   
-    if ((srok < 0) || (srok > 365) || (summa < 10000))
-    {
+
+int check(int summa, int srok) {
+    if ((srok < 0) || (srok > 365) || (summa < 10000)) {
         return 0;
     }
     return 1;
 }
-
--- src/main.c
+++ src/main.c
@@ -5,25 +5,22 @@
 #include "deposit.h"
 
 int main() {
-    int summa ;
+    int summa;
     int srok;// Размер вклада и срок
     int k = 0;
-    
-    printf("Введите сумму не менее 10000р и срок не более 365 дней \n");
-    scanf("%d",&summa);
-    scanf("%d",&srok);
-    
-    k = check (summa,srok);
 
-    if (k == 1)
-      {
-      int sum = Bklad (summa,srok);
-      printf("Сумма равна = %d\n",sum);
-      }
-    else {
-      printf ("Сумма и срок введены неверно \n");
-      }
+    printf("Введите сумму не менее 10000р и срок не более 365 дней \n");
+    scanf("%d", &summa);
+    scanf("%d", &srok);
+
+    k = check(summa, srok);
+
+    if (k == 1) {
+        int sum = Bklad(summa, srok);
+        printf("Сумма равна = %d\n", sum);
+    } else {
+        printf("Сумма и срок введены неверно \n");
+    }
     return 0;
 }
-    
 
